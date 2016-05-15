8898cec647eb46f2822a795b4be803fe0a9f8a7a
Josh <jalexy12@gmail.com>
2016-05-15 13:14:28 -0400

    Fixing spacing

--- examples/chess_validator/lib/validators/validator.rb
+++ examples/chess_validator/lib/validators/validator.rb
@@ -8,8 +8,10 @@ class Validator
 	def run_validations
 		@moves.each do | move |
 			start_pos, end_pos = move.split(" ")
+			
 			start_x, start_y = MoveSplitter.new(start_pos).coords
 			end_x, end_y = MoveSplitter.new(end_pos).coords
+
 			puts @board.can_move?(start_x, start_y, end_x, end_y)
 		end
 	end
